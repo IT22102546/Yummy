ff6f799e940bc52d3ef10abf30e48ed66feb93b9
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-05-25 22:05:21 +0530

    Remove header form Signin and sign up and added bg images

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,29 @@
-import { BrowserRouter , Route , Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import LogIn from "./pages/LogIn"
-import Register from "./pages/Register"
-import Header from "./components/Header"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
-export default function App() {
+function App() {
+  const location = useLocation();
+  const hideHeaderPaths = ["/sign-in", "/sign-up"];
+
+  return (
+    <div>
+      {!hideHeaderPaths.includes(location.pathname.toLowerCase()) && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<LogIn />} />
+        <Route path="/sign-up" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default function WrappedApp() {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/sign-in" element={<LogIn/>}/>
-      <Route path="/sign-up" element={<Register/>}/>
-      </Routes>
+      <App />
     </BrowserRouter>
-  )
+  );
 }
